70a676a95527d9e54564bbc4d1348a18f5fb51b6
Gabriel Paiva <125466410+gabriel-paiva@users.noreply.github.com>
2024-09-09 11:53:47 +0000

    Aplicativo - Controle gênero musical

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -6,8 +8,37 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule, FormsModule], //CommonModule -> para funcionar o *ngFor
 })
 export class HomePage {
-  constructor() {}
+  public nome = "";
+
+  public items: any[] = [ //any -> os itens podem ser qualquer tipo
+    {
+      titulo: "Forró"
+    },
+    {
+      titulo: "Rock"
+    },
+    {
+      titulo: "Sertanejo"
+    },
+    {
+      titulo: "Funk"
+    }
+  ]
+
+  constructor() { }
+
+  adicionar() {
+    //Adicionando no array
+    this.items.push(
+      {
+        titulo: this.nome
+      }
+    );
+
+    //Limpando campo nome
+    this.nome = "";
+  }
 }
